cd737f2738a5482edff846ddb5902e50bdeb29a9
rishabh259jain <55151782+rishabh259jain@users.noreply.github.com>
2020-08-19 15:21:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-console.log("hello from the js file");
-let target = Math.floor(Math.random()*10 +1);
+let target = Math.floor(Math.random()*10 + 1);
 let guess = parseInt(prompt("enter a number"));
-while(guess != target){
+while(guess !== target){
+    if(Number.isNaN(guess)){
+        guess=parseInt(prompt("please enter a VALID NUMBER !!"));
+        continue;
+    }
     if (guess<target){
-        guess = prompt("IS TOO LOW !! \n please try again");
+        guess = parseInt(prompt("IS TOO LOW !! \n please try again"));
     }
     else {
-        guess = prompt("IS TOO HIGH !! \n please try again");
+        guess = parseInt(prompt("IS TOO HIGH !! \n please try again"));
     }
 }
 alert("CONGRATS, YOU WIN!!");
